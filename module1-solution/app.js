b73ea4326e892296f0aeb73a1c00f9e75cf264b6
Pietro <pietrosavastano@gmail.com>
2016-12-20 16:54:16 +0100

    Module 1 - Solution

---
+++ module1-solution/app.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+  angular.module('LunchCheck',[]).
+  controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.lunchCheck = function(){
+                          var lunchMenu = $scope.lunchMenu;
+                          if(lunchMenu!=undefined && lunchMenu!=''){
+                            if(countElement(lunchMenu)<=3)
+                              $scope.message = 'Enjoy!';
+                            else
+                              $scope.message = 'Too much!';
+                            $scope.color='green';
+                          }else{
+                            $scope.message = 'Please enter data first';
+                            $scope.color='red';
+                          }
+                        };
+  }
+  function countElement(string){
+    var count = 0;
+    var list = string.split(',');
+    for(var i=0;i<list.length;i++){
+      //NOT consider an empty item
+      if(list[i].trim()!='')
+        count++;
+    };
+    return count;
+  }
+})();
